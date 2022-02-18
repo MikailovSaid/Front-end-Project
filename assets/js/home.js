d365124e2be7040b07274ff046a5346128706740
Said Mikailov <saidgm@code.edu.az>
2022-02-18 19:37:30 +0400

    left side shop page created

--- assets/js/home.js
+++ assets/js/home.js
@@ -33,3 +33,39 @@ $('.owl-carousel').owlCarousel({
       }
   }
 })
+
+$(function (){
+  let openBtn = $(".open-btn");
+  openBtn.click(function () {
+    if ($(this).parent().parent().next().hasClass("d-none")) {
+      $(this).parent().parent().next().removeClass("d-none");
+      $(this).parent().parent().parent().parent().addClass("scroll");
+      $(this).html("<i class='fa-solid fa-minus'></i>");
+    }else{
+      $(this).parent().parent().next().addClass("d-none");
+      $(this).parent().parent().parent().parent().removeClass("scroll");
+      $(this).html("<i class='fa-solid fa-plus'></i>");
+    }
+  })
+})
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+$(function () {
+  let openDropBtn = $(".open-drop-btn");
+  openDropBtn.click(function () {
+    if ($(this).next().hasClass("d-none")) {
+      $(this).next().removeClass("d-none");
+      $(this).children().attr("class","fa-solid fa-angle-up");
+    }else{
+      $(this).next().addClass("d-none");
+      $(this).children().attr("class","fa-solid fa-angle-down");
+    }
+  })
+})
